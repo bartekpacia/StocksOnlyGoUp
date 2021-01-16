ae38734c1691dc43adb51894031bab52ccee06fd
Bartek Pacia <barpac02@gmail.com>
2021-01-16 13:22:51 +0100

    add basic CLI functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+### Not v2 really
+
+
+Dependencies:
+- [click v7.0](https://click.palletsprojects.com/en/7.x/)
--- parse.py
+++ parse.py
@@ -1,12 +1,49 @@
 import json
+import os
+import requests
 
-with open("query.json", "r") as file:
-    query = json.load(file)
+import click
+import dotenv
 
-series = query["Time Series (Daily)"]
+dotenv.load_dotenv(verbose=True)
 
-for k, date in enumerate(series):
-    key_open = "1. open"
-    value_open = series[date][key_open]
+API_KEY = None
+MAIN_URL = "https://www.alphavantage.co"
+QUERY_URL = MAIN_URL + "/query"
 
-    print(f"{date}: {key_open}: {value_open}")
+
+@click.command()
+@click.option("--company", type=str, help="company to scrape data")
+def main(company: str):
+    API_KEY = os.getenv("AV_API_KEY")
+
+    if not API_KEY:
+        print("error: API_KEY is null")
+        exit(1)
+
+    if not company:
+        print("error: company is null")
+        exit(1)
+
+    query_params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": company,
+        "apikey": API_KEY,
+    }
+
+    response = requests.get(QUERY_URL, params=query_params)
+
+    print(f"status: {response.status_code}, url: {response.url}")
+    response_data = response.json()
+
+    series = response_data["Time Series (Daily)"]
+
+    for k, date in enumerate(series):
+        key_open = "1. open"
+        value_open = series[date][key_open]
+
+        print(f"{date}: {key_open}: {value_open}")
+
+
+if __name__ == "__main__":
+    main()

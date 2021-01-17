0660ff86b52f5ede764a34bfa15a47e3e5672a0d
Bartek Pacia <barpac02@gmail.com>
2021-01-17 10:25:02 +0100

    lol

--- app.py
+++ app.py
@@ -33,26 +33,6 @@ def upload_file_via_presigned_url():
     # The pipeline will now run
     return f"pipeline is running, id: {job_id}"
 
-@app.route("/upload_via_presigned_url")
-def upload_file_via_presigned_url():
-    # First, we need to get pre-signed url
-    r = requests.post("https://api2.dropbase.io/v1/pipeline/generate_presigned_url", data={"token": TOKEN})
-    if r.status_code != 200:  # Something failed
-        return f"error, status: {r.status_code}, msg: {r.json}"
-
-    presigned_url = r.json()["upload_url"]  # Link to upload a file
-    job_id = r.json()["job_id"]  # Job_id to see the status of the pipeline once the file is uploaded
-
-    # Now we upload the file
-    r = requests.put(presigned_url,
-                     data=open("data.json", "rb"))  # replace NHkJR6qjRu8kkRSk8zHiGt.csv with your file
-
-    if r.status_code != 200:  # Failed to upload and run pipeline
-        return f"error, status: {r.status_code}, msg: {r.json}"
-
-    # The pipeline will now run
-    return f"pipeline is running, id: {job_id}"
-
 
 @app.route("/data", methods=["GET"])
 def get_data():
